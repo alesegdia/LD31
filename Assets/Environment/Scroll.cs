416a8396f3693e662d2e0d4f4f15e39c73ae8270
alesegdia <alesegdia@gmail.com>
2014-12-06 12:58:22 +0100

    Character moving.

---
+++ Assets/Environment/Scroll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Scroll : MonoBehaviour {
+
+	public Vector2 times = new Vector2(0,0);
+    public GameObject scrolledSprite;
+    public Vector2 speed = new Vector2(3, 0);
+    private Vector2 offset = new Vector2(0,0);
+    private List<GameObject> instances = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		for( int x = 0; x < times.x; x++ )
+        {
+			for( int y = 0; y < times.y; y++ )
+            {
+				Vector3 off = new Vector3( scrolledSprite.renderer.bounds.size.x * x, scrolledSprite.renderer.bounds.size.y * y, 0 );
+                instances.Add(((GameObject)Instantiate(scrolledSprite, this.transform.position + off, Quaternion.identity)));
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach( GameObject inst in instances )
+        {
+			offset += speed * Time.deltaTime;
+			inst.renderer.material.SetTextureOffset("_MainTex", offset);
+        }
+	}
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 speed;
+    public Vector2 maxSpeed;
+    Animator animator;
+    public GameObject controlledGameobject;
+    public GameObject graphicGameobject;
+
+	// Use this for initialization
+	void Start () {
+        animator = graphicGameobject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// handle horizontal and vertical input forces
+        float hor, ver;
+        hor = ver = 0;
+		if( Input.GetKey(KeyCode.Space) )  ver = 1;
+		if( Input.GetKey(KeyCode.A) ) hor = -1;
+		if( Input.GetKey(KeyCode.D) ) hor = 1;
+        controlledGameobject.rigidbody2D.AddForce(new Vector2(hor * speed.y, ver * speed.y));
+
+		// cap
+		Vector2 vel = controlledGameobject.rigidbody2D.velocity;
+        if (vel.y > maxSpeed.y)
+        {
+            Debug.Log(vel.y);
+            vel.y = maxSpeed.y;
+        }
+		if (Mathf.Abs(vel.x) > maxSpeed.x) vel.x = maxSpeed.x * Mathf.Sign(vel.x);
+        controlledGameobject.rigidbody2D.velocity = vel;
+        animator.SetFloat("Vertical Speed", controlledGameobject.rigidbody2D.velocity.y);
+	}
+}
